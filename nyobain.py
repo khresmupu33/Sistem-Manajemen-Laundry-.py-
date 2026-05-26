15c8ce1e6da85c75f485fe2b6d8d38d1c3f8cd82
Khresmupu <khresmupu@gmail.com>
2026-05-26 17:46:12 +0700

    Ver1

---
+++ nyobain.py
@@ -0,0 +1,25 @@
+# File : nyobain.py 
+# Penulis :
+# Khresna Mulia Putra
+# Steven Evan Winardi
+# Adrian El Mahalli
+# Program pengatur layanan laundry ASEK
+# Kamus Data
+
+
+masukan = 0
+
+def main():
+    while input != 5:
+        print("1. Penerimaan Laundry Baru (Input Data)")
+        print("2. Cek / Ubah Status Cucian (Monitoring)")
+        print("3. Pengambilan & Pembayaran (Transaksi Selesai)")
+        print("4. Laporan Keuangan Toko (Rekap Data)")
+        print("5. Keluar Aplikasi")
+        masukan = int(input("Masukkan pilihan menu (1-5): "))
+
+
+    return 0
+
+if __name__ == '__main__':    
+    main()
